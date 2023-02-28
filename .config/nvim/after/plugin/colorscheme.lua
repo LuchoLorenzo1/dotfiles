3a77c2408097b50109e31debe18274b191cdfd71
Luciano Lorenzo <lucianoalorenzo1@gmail.com>
2023-02-27 21:57:09 -0300

    deleting pycache, chaging colorscheme

--- .config/nvim/after/plugin/colorscheme.lua
+++ .config/nvim/after/plugin/colorscheme.lua
@@ -39,10 +39,18 @@ vim.api.nvim_create_user_command("Colorscheme", function()
 	Color(colorscheme)
 end, {})
 
-require('noirbuddy').setup {
-  preset = 'slate',
-}
+-- require('noirbuddy').setup {
+--   preset = 'slate',
+-- }
 -- transparent('noirbuddy')
+vim.cmd.colorscheme('gruvbox')
 
-vim.api.nvim_set_hl(0, "VimwikiLink", {link="@variable.builtin"})
-
+-- vim.api.nvim_set_hl(0, "VimwikiLink", {link="@variable.builtin"})
+vim.api.nvim_set_hl(0, "SignColumn", {link="GruvboxBg1"})
+vim.api.nvim_set_hl(0, "GitSignsAdd", {bg="none", ctermfg=142, fg="#b8bb26"})
+vim.api.nvim_set_hl(0, "GitSignsChange", {bg="none", ctermfg=108, fg="#8ec07c"})
+vim.api.nvim_set_hl(0, "GitSignsDelete", {bg="none", ctermfg=167, fg="#fb4934"})
+vim.api.nvim_set_hl(0, "GruvboxYellowSign", {ctermfg=214, ctermbg="none", fg="#fabd2f"})
+vim.api.nvim_set_hl(0, "GruvboxRedSign", {ctermfg=167, ctermbg="none", fg="#fb4934"})
+vim.api.nvim_set_hl(0, "GruvboxAquaSign", {ctermfg=108, ctermbg="none", fg="#8ec07c"})
+vim.api.nvim_set_hl(0, "GruvboxBlueSign", {ctermfg=109, ctermbg="none", fg="#83a598"})
