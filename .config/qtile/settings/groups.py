1dd699d71f268fb0aa9dfa0707758e03ee7febae
Luciano Lorenzo <lucianoalorenzo1@gmail.com>
2023-11-04 11:24:17 -0300

    adding letter groups to qtile

--- .config/qtile/settings/groups.py
+++ .config/qtile/settings/groups.py
@@ -3,14 +3,15 @@ from libqtile.command import lazy
 from .keys import mod, keys
 
 groups = [Group(i) for i in ("6", "7", "8", "9", "0")]
+ext_keys = ["y", "u", "i", "o", "p"]
 
 for i, group in enumerate(groups):
     actual_key = str(i + 1)
     keys.extend(
         [
-            # Switch to workspace N
             Key([mod], group.name, lazy.group[group.name].toscreen()),
-            # Send window to workspace N
             Key([mod, "shift"], group.name, lazy.window.togroup(group.name)),
+            Key([mod], ext_keys[i], lazy.group[group.name].toscreen()),
+            Key([mod, "shift"], ext_keys[i], lazy.window.togroup(group.name)),
         ]
     )
