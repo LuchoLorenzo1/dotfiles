d69417d617a4055ebb39fb508e26345951421748
Luciano Lorenzo <lucianoalorenzo1@gmail.com>
2023-11-04 11:24:40 -0300

    update post_install.sh

--- post_install.sh
+++ post_install.sh
@@ -21,13 +21,28 @@ fi
 clear
 echo -e "${yellowColor}\n ENTER para empezar instalando lo mas importante: (git xorg xorg-xinit base-devel) \n${endColor}"
 if [[ $response =~ [yY] ]] || [ -z $response ]; then
+	sudo pacman -Sy
 	sudo pacman --noconfirm -S git xorg xorg-xinit base-devel
+fi
+
+clear
+echo -e "${yellowColor}\n Clonar dotfiles \n${endColor}"; read response
+if [[ $response =~ [yY] ]] || [ -z $response ]; then
+	git clone --bare https://github.com/LuchoLorenzo1/dotfiles $HOME/.cfg
+
+	mkdir -p .config-backup && \
+	/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | \
+	xargs -I{} mv {} .config-backup/{}
+
+	/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME checkout
+
+	/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME config --local status.showUntrackedFiles no
 fi
 
 clear
 echo -e "${yellowColor}Instalar drivers? [Y/n]${endColor}"; read response
 if [[ $response =~ [yY] ]] || [ -z $response ]; then
-	pacman --noconfirm -S mesa vulkan-radeon xf86-video-amdgpu
+	sudo pacman --noconfirm -S mesa vulkan-radeon xf86-video-amdgpu
 fi
 
 clear
@@ -44,13 +59,13 @@ clear
 echo -e "${yellowColor}Instalar qtile? [Y/n]${endColor}"; read response
 if [[ $response =~ [yY] ]] || [ -z $response ]; then
 	pacman --noconfirm -S qtile
-	pip install --no-input psutil dbus-next
+	pip install --break-system-packages--no-input psutil dbus-next pulsectl_asyncio
 fi
 
 echo -e "${yellowColor}Instalar cosas de neovim? [Y/n]${endColor}"; read response
 if [[ $response =~ [yY] ]] || [ -z $response ]; then
 	pacman --noconfirm -S neovim xclip
-	pip install --no-input pynvim black flake8
+	pip install --break-system-packages --no-input pynvim black flake8
 	npm install -g neovim mathjax prettier
 fi
 
@@ -61,11 +76,15 @@ fi
 
 echo -e "${yellowColor}Instalar paquetes esenciales? [Y/n]${endColor}"; read response
 if [[ $response =~ [yY] ]] || [ -z $response ]; then
-	pacman --noconfirm -S pulseaudio pavucontrol vifm chromium tmux rofi lsd bat starship alacritty fzf feh redshift picom ttf-cascadia-code ttf-nerd-fonts-symbols
-	yay -S dragon-drop
+	sudo pacman --noconfirm -S pulseaudio pavucontrol vifm chromium tmux rofi lsd bat starship alacritty fzf feh redshift picom ttf-cascadia-code ttf-nerd-fonts-symbols
+	yay -S  --noconfirm dragon-drop
 	git config --global credential.helper store
 	git config --global user.email "lucianoalorenzo1@gmail.com"
 	git config --global user.name "Luciano Lorenzo"
+fi
+
+echo -e "${yellowColor}Instalar y configurar docker esenciales? [Y/n]${endColor}"; read response
+if [[ $response =~ [yY] ]] || [ -z $response ]; then
 	sudo pacman --noconfirm -S docker docker-compose
 	sudo usermod -aG docker lucho
 	systemctl enable docker
@@ -73,8 +92,7 @@ fi
 
 echo -e "${yellowColor}Instalar paquetes NO TAN esenciales? [Y/n]${endColor}"; read response
 if [[ $response =~ [yY] ]] || [ -z $response ]; then
-	pacman --noconfirm -S flameshot discord htop-vim pinta ueberzug unzip poppler ripgrep fd zip vlc
-
+	yay --noconfirm -S flameshot discord pinta ueberzug unzip poppler ripgrep fd zip vlc
 	sleep 1
 fi
 
@@ -82,4 +100,9 @@ echo -e "${yellowColor}Instalar paquetes de data science? [Y/n]${endColor}"; rea
 if [[ $response =~ [yY] ]] || [ -z $response ]; then
 	pip install --no-input numpy pandas scipy matplotlib jupyter geopandas scikit-learn
 	sleep 1
+fi
+
+echo -e "${yellowColor}Instalar Rust? [Y/n]${endColor}"; read response
+if [[ $response =~ [yY] ]] || [ -z $response ]; then
+	curl --proto '=https' --tlsv1.2 https://sh.rustup.rs -sSf | sh
 fi
